9ba671e45604ed8205e663f9c6190e218359da8f
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:20:21 +0200

    Initialize issue-764-close-shield-button

---
+++ src/screens/home/components/BaseHomeView.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {StyleSheet, ScrollView} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Box, Header, Icon, IconName} from 'components';
+
+interface BaseHomeViewProps {
+  children?: React.ReactNode;
+  iconName?: IconName;
+}
+
+export const BaseHomeView = ({children, iconName}: BaseHomeViewProps) => {
+  return (
+    <>
+      <SafeAreaView style={styles.flex}>
+        <Header />
+      </SafeAreaView>
+      <ScrollView contentContainerStyle={[styles.scrollContainer]} bounces={false}>
+        <SafeAreaView style={styles.flex}>
+          <Box width="100%" justifyContent="flex-start" marginBottom="-l">
+            <Box style={{...styles.primaryIcon}}>
+              <Icon name={iconName} height={120} width={150} />
+            </Box>
+          </Box>
+          <Box width="100%" flex={1} alignItems="flex-start" justifyContent="flex-start" paddingHorizontal="m">
+            {children}
+          </Box>
+        </SafeAreaView>
+      </ScrollView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  primaryIcon: {marginLeft: -40, marginBottom: 30},
+  flex: {
+    flex: 0,
+  },
+  scrollContainerWithAnimation: {
+    marginTop: -100,
+  },
+  scrollContainer: {
+    minHeight: '90%',
+    maxWidth: 600,
+    alignItems: 'flex-start',
+  },
+  animationBase: {
+    marginBottom: -100,
+  },
+});
---
+++ src/screens/home/views/OverlayView.tsx
@@ -0,0 +1,208 @@
+import React, {useCallback} from 'react';
+import Animated, {sub, abs} from 'react-native-reanimated';
+import {Box, InfoBlock, BoxProps, InfoButton, BottomSheetBehavior, Icon} from 'components';
+import {useI18n, I18n} from 'locale';
+import {Linking, Platform, TouchableOpacity, StyleSheet, View} from 'react-native';
+import {
+  SystemStatus,
+  useExposureStatus,
+  useStartExposureNotificationService,
+} from 'services/ExposureNotificationService';
+import {useNavigation} from '@react-navigation/native';
+import {daysBetween} from 'shared/date-fns';
+import {pluralizeKey} from 'shared/pluralization';
+import {ScrollView} from 'react-native-gesture-handler';
+import {useAccessibilityService} from 'services/AccessibilityService';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+import {InfoShareView} from './InfoShareView';
+import {StatusHeaderView} from './StatusHeaderView';
+
+const SystemStatusOff = ({i18n}: {i18n: I18n}) => {
+  const startExposureNotificationService = useStartExposureNotificationService();
+  const onPress = () => {
+    if (Platform.OS === 'android') {
+      startExposureNotificationService();
+      return;
+    }
+    return toSettings();
+  };
+  const toSettings = useCallback(() => {
+    Linking.openSettings();
+  }, []);
+
+  return (
+    <InfoButton
+      title={i18n.translate('OverlayOpen.ExposureNotificationCardAction')}
+      text={i18n.translate('OverlayOpen.ExposureNotificationCardBody')}
+      color="danger25Background"
+      variant="danger50Flat"
+      internalLink
+      onPress={onPress}
+    />
+  );
+};
+
+const BluetoothStatusOff = ({i18n}: {i18n: I18n}) => {
+  return (
+    <InfoBlock
+      titleBolded={i18n.translate('OverlayOpen.BluetoothCardAction')}
+      backgroundColor="danger25Background"
+      color="bodyText"
+      button={{
+        text: '',
+        action: () => {},
+      }}
+      text={i18n.translate('OverlayOpen.BluetoothCardBody')}
+      showButton={false}
+    />
+  );
+};
+
+const NotificationStatusOff = ({action, i18n}: {action: () => void; i18n: I18n}) => {
+  return (
+    <InfoButton
+      title={i18n.translate('OverlayOpen.NotificationCardStatus')}
+      color="mainBackground"
+      internalLink
+      text={i18n.translate('OverlayOpen.NotificationCardBody')}
+      onPress={action}
+      variant="bigFlatNeutralGrey"
+    />
+  );
+
+  /*
+  return (
+    <InfoBlock
+      icon="icon-notifications"
+      title={i18n.translate('OverlayOpen.NotificationCardStatus')}
+      titleBolded={i18n.translate('OverlayOpen.NotificationCardStatusOff')}
+      text={i18n.translate('OverlayOpen.NotificationCardBody')}
+      button={{text: i18n.translate('OverlayOpen.NotificationCardAction'), action}}
+      backgroundColor="infoBlockNeutralBackground"
+      color="overlayBodyText"
+    />
+  );
+  */
+};
+
+const ShareDiagnosisCode = ({i18n, isBottomSheetExpanded}: {i18n: I18n; isBottomSheetExpanded: boolean}) => {
+  const navigation = useNavigation();
+  const [exposureStatus] = useExposureStatus();
+
+  if (exposureStatus.type === 'diagnosed') {
+    const daysLeft = daysBetween(new Date(), new Date(exposureStatus.cycleEndsAt)) - 1;
+    const bodyText =
+      i18n.translate('OverlayOpen.EnterCodeCardBodyDiagnosed') +
+      i18n.translate(pluralizeKey('OverlayOpen.EnterCodeCardDiagnosedCountdown', daysLeft), {number: daysLeft});
+    return (
+      <InfoBlock
+        focusOnTitle={isBottomSheetExpanded}
+        titleBolded={i18n.translate('OverlayOpen.EnterCodeCardTitleDiagnosed')}
+        text={bodyText}
+        button={{
+          text: '',
+          action: () => {},
+        }}
+        backgroundColor="infoBlockNeutralBackground"
+        color="bodyText"
+        showButton={false}
+      />
+    );
+  }
+  return (
+    <InfoBlock
+      focusOnTitle={isBottomSheetExpanded}
+      titleBolded={i18n.translate('OverlayOpen.EnterCodeCardTitle')}
+      text={i18n.translate('OverlayOpen.EnterCodeCardBody')}
+      button={{
+        text: i18n.translate('OverlayOpen.EnterCodeCardAction'),
+        action: () => navigation.navigate('DataSharing'),
+      }}
+      backgroundColor="infoBlockNeutralBackground"
+      color="bodyText"
+      showButton
+    />
+  );
+};
+
+const AccessibleView = ({children}: {children: React.ReactNode}) => {
+  const accessibilityService = useAccessibilityService();
+
+  return accessibilityService.isScreenReaderEnabled ? (
+    <ScrollView style={styles.content} showsVerticalScrollIndicator={false}>
+      {children}
+    </ScrollView>
+  ) : (
+    <View style={styles.content}>{children}</View>
+  );
+};
+
+interface Props extends Pick<BoxProps, 'maxWidth'> {
+  status: SystemStatus;
+  notificationWarning: boolean;
+  turnNotificationsOn: () => void;
+  bottomSheetBehavior: BottomSheetBehavior;
+}
+
+export const OverlayView = ({status, notificationWarning, turnNotificationsOn, bottomSheetBehavior}: Props) => {
+  const i18n = useI18n();
+
+  return (
+    <Animated.View style={{opacity: abs(sub(bottomSheetBehavior.callbackNode, 1))}}>
+      <AccessibleView>
+        <SafeAreaView>
+          <Box>
+            <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={bottomSheetBehavior.collapse}
+              style={styles.collapseButton}
+              accessibilityLabel={i18n.translate('BottomSheet.Collapse')}
+              accessibilityRole="button"
+            >
+              <Icon name="sheet-handle-bar-close" size={36} />
+            </TouchableOpacity>
+
+            <Box marginBottom="s">
+              <StatusHeaderView enabled={status === SystemStatus.Active} />
+            </Box>
+            <Box marginBottom="m" marginTop="s" marginHorizontal="m">
+              <ShareDiagnosisCode isBottomSheetExpanded={bottomSheetBehavior.isExpanded} i18n={i18n} />
+            </Box>
+            {(status === SystemStatus.Disabled || status === SystemStatus.Restricted) && (
+              <Box marginBottom="m" marginHorizontal="m">
+                <SystemStatusOff i18n={i18n} />
+              </Box>
+            )}
+            {status === SystemStatus.BluetoothOff && (
+              <Box marginBottom="m" marginHorizontal="m">
+                <BluetoothStatusOff i18n={i18n} />
+              </Box>
+            )}
+            {notificationWarning && (
+              <Box marginBottom="m" marginHorizontal="m">
+                <NotificationStatusOff action={turnNotificationsOn} i18n={i18n} />
+              </Box>
+            )}
+            <Box marginBottom="m" marginHorizontal="m">
+              <InfoShareView />
+            </Box>
+          </Box>
+        </SafeAreaView>
+      </AccessibleView>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  content: {
+    marginTop: -26,
+  },
+  collapseButton: {
+    height: 48,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginBottom: -10,
+  },
+});

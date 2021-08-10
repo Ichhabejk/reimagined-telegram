2f23bd81686a9562884f37f3dd978cebdc5c4564
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:21:17 +0200

    Initialize snyk-upgrade-f6662f3ca043cd6826e83a23bf2ae389

---
+++ src/screens/datasharing/DataSharing.tsx
@@ -0,0 +1,80 @@
+import React, {useCallback, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {Box, Toolbar} from 'components';
+import {StyleSheet, Alert, ScrollView} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useI18n} from 'locale';
+import {useExposureStatus} from 'services/ExposureNotificationService';
+
+import {Step1} from './views/Step1';
+import {FormView} from './views/FormView';
+import {ConsentView} from './views/ConsentView';
+
+export const DataSharingScreen = () => {
+  const navigation = useNavigation();
+  const i18n = useI18n();
+  const close = useCallback(() => navigation.goBack(), [navigation]);
+  const [exposureStatus] = useExposureStatus();
+  const [codeValue, setCodeValue] = useState('');
+  const handleChange = useCallback(text => setCodeValue(text), []);
+  const acceptStep1 = useCallback(() => setIsConfirmedStep1(true), []);
+
+  // if keySubmissionStatus is None we need the 1-time code, otherwise we should go right to consent
+  const [isVerified, setIsVerified] = useState(exposureStatus.type === 'diagnosed');
+  const [isConfirmedStep1, setIsConfirmedStep1] = useState(false);
+  const onErrorForm = () => {
+    Alert.alert(i18n.translate('DataUpload.FormView.ErrorTitle'), i18n.translate('DataUpload.FormView.ErrorBody'), [
+      {text: i18n.translate('DataUpload.FormView.ErrorAction')},
+    ]);
+    setIsVerified(false);
+  };
+
+  const onErrorConsent = () => {
+    Alert.alert(
+      i18n.translate('DataUpload.ConsentView.ErrorTitle'),
+      i18n.translate('DataUpload.ConsentView.ErrorBody'),
+      [{text: i18n.translate('DataUpload.FormView.ErrorAction')}],
+    );
+  };
+
+  const handleVerify = useCallback(async () => {
+    setIsVerified(true);
+  }, []);
+
+  const handleUploaded = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  const getContent = () => {
+    if (isVerified) {
+      return <ConsentView onSuccess={handleUploaded} onError={onErrorConsent} />;
+    } else if (!isVerified && isConfirmedStep1) {
+      return <FormView value={codeValue} onChange={handleChange} onSuccess={handleVerify} onError={onErrorForm} />;
+    } else {
+      return <Step1 onSuccess={acceptStep1} />;
+    }
+  };
+
+  return (
+    <Box backgroundColor="overlayBackground" flex={1}>
+      <SafeAreaView style={styles.flex}>
+        <Toolbar
+          title=""
+          navIcon="icon-back-arrow"
+          navText={i18n.translate('DataUpload.Cancel')}
+          navLabel={i18n.translate('DataUpload.Cancel')}
+          onIconClicked={close}
+        />
+        <ScrollView style={styles.flex} keyboardShouldPersistTaps="handled">
+          {getContent()}
+        </ScrollView>
+      </SafeAreaView>
+    </Box>
+  );
+};
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+});

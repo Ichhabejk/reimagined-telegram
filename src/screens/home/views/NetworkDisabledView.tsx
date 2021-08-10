2bc0521efd5e7a8d1af52d656a4504f4980035b5
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:18:21 +0200

    Initialize smaller-images

---
+++ src/screens/home/views/NetworkDisabledView.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {useI18n} from '@shopify/react-i18n';
+import {Box, Text, LastCheckedDisplay, Icon} from 'components';
+
+import {BaseHomeView} from '../components/BaseHomeView';
+
+export const NetworkDisabledView = () => {
+  const [i18n] = useI18n();
+  return (
+    <BaseHomeView>
+      <Box marginBottom="l" marginLeft="-xxl">
+        <Icon name="icon-bluetooth-disabled" size={152} />
+      </Box>
+      <Text variant="bodyTitle" color="bodyText" marginBottom="m" accessibilityRole="header">
+        {i18n.translate('Home.NoConnectivity')}
+      </Text>
+      <Text variant="bodyText" color="bodyText" marginBottom="m">
+        {i18n.translate('Home.NoConnectivityDetailed')}
+      </Text>
+      <LastCheckedDisplay />
+      <Box marginBottom="xxl" />
+    </BaseHomeView>
+  );
+};

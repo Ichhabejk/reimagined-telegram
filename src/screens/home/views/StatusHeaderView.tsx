f5b49c33b375a3722bf223e98b8654cbcdb81ed5
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:19:24 +0200

    Initialize snyk-upgrade-334e9bfe7c80130031c862ad9c304998

---
+++ src/screens/home/views/StatusHeaderView.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Box, Text} from 'components';
+import {useI18n} from '@shopify/react-i18n';
+
+interface Props {
+  enabled: boolean;
+}
+export const StatusHeaderView = ({enabled}: Props) => {
+  const [i18n] = useI18n();
+  const color = enabled ? 'statusSuccess' : 'statusError';
+  return (
+    <Box justifyContent="center" flexDirection="row" alignItems="flex-start" paddingHorizontal="m">
+      <Text>
+        <Text variant="overlayTitle" color={color}>
+          {i18n.translate('OverlayClosed.SystemStatus')}
+        </Text>
+        <Text variant="overlayTitle" color={color} fontWeight="bold">
+          {enabled ? i18n.translate('OverlayClosed.SystemStatusOn') : i18n.translate('OverlayClosed.SystemStatusOff')}
+        </Text>
+      </Text>
+    </Box>
+  );
+};

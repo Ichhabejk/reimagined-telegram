5c0afcf62f97a703be4bf75cae73d63bc0f22e94
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:21:45 +0200

    Initialize feature/20200730-code-refactoring

---
+++ src/screens/onboarding/OnboardingContent.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {StyleSheet, Platform} from 'react-native';
+import {Box} from 'components';
+import ScrollView from 'rn-faded-scrollview';
+import {useOrientation} from 'shared/useOrientation';
+
+import {Start} from './views/Start';
+import {Anonymous} from './views/Anonymous';
+import {HowItWorks} from './views/HowItWorks';
+import {Permissions} from './views/Permissions';
+import {Region} from './views/Region';
+import {ItemViewProps} from './views/ItemView';
+import {PartOf} from './views/PartOf';
+
+export type OnboardingKey = 'step-1' | 'step-2' | 'step-3' | 'step-4' | 'step-5' | 'step-6';
+
+export const onboardingData: OnboardingKey[] = ['step-1', 'step-2', 'step-3', 'step-4', 'step-5', 'step-6'];
+
+const viewComponents: {[key in OnboardingKey]: React.ComponentType<Pick<ItemViewProps, 'isActive'>>} = {
+  'step-1': Start,
+  'step-2': Anonymous,
+  'step-3': HowItWorks,
+  'step-4': PartOf,
+  'step-5': Permissions,
+  'step-6': Region,
+};
+
+export interface OnboardingContentProps {
+  item: OnboardingKey;
+  isActive: boolean;
+}
+
+export const OnboardingContent = ({item, isActive}: OnboardingContentProps) => {
+  const Item = viewComponents[item];
+  const {orientation} = useOrientation();
+  const rightPadding = orientation === 'landscape' && Platform.OS === 'ios' ? 'xxl' : 'm';
+  const rightMargin = orientation === 'landscape' && Platform.OS === 'ios' ? 'l' : 'none';
+  return (
+    <ScrollView
+      testID={`${item}ScrollView`}
+      fadeSize={50}
+      fadeColors={['rgba(255, 255, 255, 0.18)', 'rgba(255, 255, 255, 0.6)', 'rgba(255, 255, 255, 0.9)']}
+      style={styles.flex}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.content}
+    >
+      <Box paddingLeft="m" paddingTop="s" paddingRight={rightPadding} marginRight={rightMargin}>
+        <Item isActive={isActive} />
+      </Box>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  content: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+});
---
+++ src/services/BackgroundSchedulerService/BackgroundSchedulerService.ts
@@ -0,0 +1,51 @@
+import BackgroundFetch from 'react-native-background-fetch';
+import {Platform} from 'react-native';
+import {TEST_MODE} from 'env';
+import {captureMessage, captureException} from 'shared/log';
+
+const BACKGROUND_TASK_ID = 'app.covidshield.exposure-notification';
+
+interface PeriodicTask {
+  (): Promise<void>;
+}
+
+// See https://github.com/cds-snc/covid-shield-mobile/issues/642#issuecomment-657783192
+const DEFERRED_JOB_INTERNVAL_IN_MINUTES = 240;
+const EXACT_JOB_INTERNVAL_IN_MINUTES = 90;
+
+const registerPeriodicTask = async (task: PeriodicTask) => {
+  BackgroundFetch.configure(
+    {
+      minimumFetchInterval: TEST_MODE ? EXACT_JOB_INTERNVAL_IN_MINUTES : DEFERRED_JOB_INTERNVAL_IN_MINUTES,
+      forceAlarmManager: TEST_MODE,
+      enableHeadless: true,
+      startOnBoot: true,
+      stopOnTerminate: false,
+    },
+    async taskId => {
+      captureMessage('runPeriodicTask', {taskId});
+      await task().catch(error => captureException('runPeriodicTask', error));
+      BackgroundFetch.finish(taskId);
+    },
+  );
+  const result = await BackgroundFetch.scheduleTask({taskId: BACKGROUND_TASK_ID, delay: 0, periodic: true}).catch(
+    () => false,
+  );
+  captureMessage('registerPeriodicTask', {result});
+};
+
+const registerAndroidHeadlessPeriodicTask = (task: PeriodicTask) => {
+  if (Platform.OS !== 'android') return;
+
+  BackgroundFetch.registerHeadlessTask(async ({taskId}) => {
+    captureMessage('runAndroidHeadlessPeriodicTask', {taskId});
+    await task().catch(error => captureException('runAndroidHeadlessPeriodicTask', error));
+    BackgroundFetch.finish(taskId);
+  });
+  captureMessage('registerAndroidHeadlessPeriodicTask');
+};
+
+export const BackgroundScheduler = {
+  registerPeriodicTask,
+  registerAndroidHeadlessPeriodicTask,
+};

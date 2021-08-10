f6e95821cbb0d2596f6728847960fe03d9f71abc
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:18:38 +0200

    Initialize tutorial-split-text

---
+++ src/services/ExposureNotificationService/ExposureNotificationServiceProvider.tsx
@@ -0,0 +1,142 @@
+import React, {createContext, useCallback, useContext, useEffect, useMemo, useState} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import {useI18n} from '@shopify/react-i18n';
+import ExposureNotification, {Status as SystemStatus} from 'bridge/ExposureNotification';
+import {AppState, AppStateStatus} from 'react-native';
+import SecureStorage from 'react-native-sensitive-info';
+import SystemSetting from 'react-native-system-setting';
+
+import {BackendInterface} from '../BackendService';
+import {BackgroundScheduler} from '../BackgroundSchedulerService';
+
+import {
+  ExposureNotificationService,
+  ExposureStatus,
+  PersistencyProvider,
+  SecurePersistencyProvider,
+} from './ExposureNotificationService';
+
+const ExposureNotificationServiceContext = createContext<ExposureNotificationService | undefined>(undefined);
+
+export interface ExposureNotificationServiceProviderProps {
+  backendInterface: BackendInterface;
+  backgroundScheduler?: typeof BackgroundScheduler;
+  exposureNotification?: typeof ExposureNotification;
+  storage?: PersistencyProvider;
+  secureStorage?: SecurePersistencyProvider;
+  children?: React.ReactElement;
+}
+
+export const ExposureNotificationServiceProvider = ({
+  backendInterface,
+  backgroundScheduler = BackgroundScheduler,
+  exposureNotification,
+  storage,
+  secureStorage,
+  children,
+}: ExposureNotificationServiceProviderProps) => {
+  const [i18n] = useI18n();
+  const exposureNotificationService = useMemo(
+    () =>
+      new ExposureNotificationService(
+        backendInterface,
+        i18n,
+        storage || AsyncStorage,
+        secureStorage || SecureStorage,
+        exposureNotification || ExposureNotification,
+      ),
+    [backendInterface, exposureNotification, i18n, secureStorage, storage],
+  );
+
+  useEffect(() => {
+    backgroundScheduler.registerPeriodicTask(() => {
+      return exposureNotificationService.updateExposureStatusInBackground();
+    });
+  }, [backgroundScheduler, exposureNotificationService]);
+
+  return (
+    <ExposureNotificationServiceContext.Provider value={exposureNotificationService}>
+      {children}
+    </ExposureNotificationServiceContext.Provider>
+  );
+};
+
+export function useExposureNotificationService() {
+  return useContext(ExposureNotificationServiceContext)!;
+}
+
+export function useStartExposureNotificationService(): () => Promise<void> {
+  const exposureNotificationService = useExposureNotificationService();
+  return useCallback(async () => {
+    await exposureNotificationService.start();
+  }, [exposureNotificationService]);
+}
+
+export function useSystemStatus(): [SystemStatus, () => void] {
+  const exposureNotificationService = useExposureNotificationService();
+  const [state, setState] = useState<SystemStatus>(exposureNotificationService.systemStatus.get());
+  const update = useCallback(() => {
+    exposureNotificationService.updateSystemStatus();
+  }, [exposureNotificationService]);
+
+  useEffect(() => {
+    return exposureNotificationService.systemStatus.observe(setState);
+  }, [exposureNotificationService.systemStatus]);
+
+  useEffect(update, [update]);
+
+  return [state, update];
+}
+
+export function useExposureStatus(): [ExposureStatus, () => void] {
+  const exposureNotificationService = useExposureNotificationService();
+  const [state, setState] = useState<ExposureStatus>(exposureNotificationService.exposureStatus.get());
+  const update = useCallback(() => {
+    exposureNotificationService.updateExposureStatus();
+  }, [exposureNotificationService]);
+
+  useEffect(() => {
+    return exposureNotificationService.exposureStatus.observe(setState);
+  }, [exposureNotificationService.exposureStatus]);
+
+  return [state, update];
+}
+
+export function useReportDiagnosis() {
+  const exposureNotificationService = useExposureNotificationService();
+  const startSubmission = useCallback(
+    (oneTimeCode: string) => {
+      return exposureNotificationService.startKeysSubmission(oneTimeCode);
+    },
+    [exposureNotificationService],
+  );
+  const fetchAndSubmitKeys = useCallback(() => {
+    return exposureNotificationService.fetchAndSubmitKeys();
+  }, [exposureNotificationService]);
+  return {
+    startSubmission,
+    fetchAndSubmitKeys,
+  };
+}
+
+export function useExposureNotificationSystemStatusAutomaticUpdater() {
+  const exposureNotificationService = useExposureNotificationService();
+  return useCallback(() => {
+    const updateStatus = async (newState: AppStateStatus) => {
+      if (newState === 'active') {
+        await exposureNotificationService.updateSystemStatus();
+        await exposureNotificationService.updateExposureStatus();
+      }
+    };
+    AppState.addEventListener('change', updateStatus);
+
+    const bluetoothListenerPromise = SystemSetting.addBluetoothListener(() => {
+      exposureNotificationService.updateSystemStatus();
+    });
+
+    return () => {
+      AppState.removeEventListener('change', updateStatus);
+      bluetoothListenerPromise.then(listener => listener.remove()).catch(() => {});
+    };
+  }, [exposureNotificationService]);
+}

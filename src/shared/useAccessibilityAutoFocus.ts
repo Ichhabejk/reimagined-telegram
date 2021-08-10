818914efc23eec610247ec37230454dbc75b0a47
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:20:37 +0200

    Initialize issue/839/special-chars

---
+++ src/shared/useAccessibilityAutoFocus.ts
@@ -0,0 +1,57 @@
+import {useLayoutEffect, useMemo, useState} from 'react';
+import {AccessibilityInfo, findNodeHandle} from 'react-native';
+
+import {createCancellableCallbackPromise} from './cancellablePromise';
+
+/**
+ * Look like there is timing issue with AccessibilityInfo.setAccessibilityFocus
+ * Ref https://github.com/react-native-community/discussions-and-proposals/issues/118
+ */
+const AUTO_FOCUS_DELAY = 500;
+
+export const focusOnElement = (elementRef: any) => {
+  const node = findNodeHandle(elementRef);
+  if (!node) {
+    return;
+  }
+  AccessibilityInfo.setAccessibilityFocus(node);
+};
+
+/**
+ * Remember to add `accessible={true}` to target component
+ * @returns [Manual focus ref , Auto focus ref]
+ */
+export const useAccessibilityAutoFocus = (isActive = true) => {
+  const [autoFocusRef, setAutoFocusRef] = useState<any>();
+
+  const {callable, cancelable} = useMemo(() => {
+    return createCancellableCallbackPromise(
+      () => AccessibilityInfo.isScreenReaderEnabled().then(delay(AUTO_FOCUS_DELAY)),
+      isScreenReaderEnabled => {
+        if (!isScreenReaderEnabled) {
+          return;
+        }
+        focusOnElement(autoFocusRef);
+      },
+    );
+  }, [autoFocusRef]);
+
+  useLayoutEffect(() => {
+    if (!autoFocusRef || !isActive) {
+      return;
+    }
+
+    callable();
+    return cancelable;
+  }, [callable, cancelable, autoFocusRef, isActive]);
+
+  return [autoFocusRef, setAutoFocusRef];
+};
+
+function delay<T>(timeout: number) {
+  return (value: T) => {
+    return new Promise<T>(function(resolve) {
+      setTimeout(resolve.bind(null, value), timeout);
+    });
+  };
+}

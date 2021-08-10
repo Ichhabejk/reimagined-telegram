6212b3c3fcc13d43bf19094ab77e2817ec57c908
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:18:36 +0200

    Initialize pre-flight-1593828020598

---
+++ src/components/BulletPointCheck.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Box, Text, Icon} from 'components';
+
+export const BulletPointCheck = ({text}: {text: string}) => {
+  return (
+    <Box flexDirection="row" marginBottom="m">
+      <Box marginTop="xxs" flex={0}>
+        <Icon size={20} name="icon-green-check" />
+      </Box>
+      <Box flex={1}>
+        <Text variant="bodyText" color="overlayBodyText" marginLeft="m">
+          {text}
+        </Text>
+      </Box>
+    </Box>
+  );
+};

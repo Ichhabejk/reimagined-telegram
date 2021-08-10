0f766281df11ff8620d4aab60b0b1f9796790cd5
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:18:16 +0200

    Initialize onboarding-images

---
+++ src/assets/privacypolicy-fr.js
@@ -0,0 +1,42 @@
+const privacy = `# Politique de confidentialité
+
+*L’application de notification d’exposition et ses serveurs ne recueillent ni ne stockent aucun de vos renseignements personnels.*
+
+Note : L'application n'a pas encore été publiée. Cette politique sera mise à jour avant sa publication, afin de fournir les informations les plus précises possibles.
+
+## Données que l’application collecte et retient
+
+- Codes générés aléatoirement par votre téléphone, des 14 derniers jours
+- Codes générés aléatoirement par d'autres téléphones près de chez vous, au cours des 14 derniers jours
+
+## Comment l’application sécurise ces données
+
+- Ces codes aléatoires ne peuvent pas être reliés à vous par l’application.
+- Votre téléphone chiffre ces données. Votre téléphone vérifiera avec auprès de vous avant de les partager avec l’application.
+
+## L’application n'a aucun moyen de connaître ou de partager
+
+- Votre localisation
+- Où vous avez été
+- Avec qui vous avez été
+- Votre identité
+- Vos contacts
+- Tout détail sur les personnes près de vous
+- Informations sur votre santé
+
+## Données partagées par l’application
+
+- *Si vous obtenez un résultat positif au test COVID-19 et que vous entrez le code unique fourni par votre prestataire de soins :*
+  * Avec votre consentement, vos codes générés aléatoirement au cours des 14 derniers jours seront partagés avec le serveur de l’application. Cela informera les personnes dont les téléphones étaient proches de vous qu’elles pussent être exposées à la COVID-19.
+  * Pendant les 13 jours qui suivent le signalement d'un test positif, l'application vous demandera la permission de partager avec le serveur vos codes générés aléatoirement ces jours-là, afin que les personnes dont vous étiez proche puissent être averties.
+- *Si une personne dont vous étiez proche reçoit un diagnostic positif à la COVID-19*, vos renseignements personnels ne sont pas partagés avec cette personne ou le serveur. La correspondance entre les codes aléatoires COVID-positifs et les codes aléatoires de votre propre téléphone se fait sur votre téléphone.
+- *Rien n'est partagé sans votre autorisation.* Même lorsque vous donnez votre autorisation, les seules informations qui sont partagées sont les codes aléatoires de votre téléphone.
+- *Rien n'est partagé avec des tiers.* Seuls l'application et les serveurs auront accès aux codes générés aléatoirement.
+
+## Suppression de vos données
+
+- Les codes générés aléatoirement à partir de votre téléphone et des autres téléphones à proximité de vous sont automatiquement supprimés 14 jours après leur génération.
+- Vous pouvez désinstaller l'application à tout moment, et les codes sur votre téléphone seront automatiquement supprimés. Si vous avez téléversé vos codes générés aléatoirement, ils seront automatiquement supprimés du serveur 14 jours après avoir été générés.
+`;
+
+export default privacy;

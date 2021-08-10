af0ca1a9f94ad3d57aa6d8ba5f48f7c456f80204
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:20:21 +0200

    Initialize pre-flight-1595197513730

---
+++ src/screens/home/components/AllSetView.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Text, TextMultiline} from 'components';
+import {useAccessibilityAutoFocus} from 'shared/useAccessibilityAutoFocus';
+
+import {BaseHomeView} from './BaseHomeView';
+
+export const AllSetView = ({
+  isBottomSheetExpanded,
+  titleText,
+  bodyText,
+}: {
+  isBottomSheetExpanded: boolean;
+  titleText: string;
+  bodyText: string;
+}) => {
+  const [, autoFocusRef] = useAccessibilityAutoFocus(!isBottomSheetExpanded);
+  return (
+    <BaseHomeView iconName="thumbs-up">
+      <Text focusRef={autoFocusRef} variant="bodyTitle" color="bodyText" marginBottom="m" accessibilityRole="header">
+        {titleText}
+      </Text>
+      <TextMultiline text={bodyText} marginBottom="m" />
+    </BaseHomeView>
+  );
+};

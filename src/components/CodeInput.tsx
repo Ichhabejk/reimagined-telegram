29875287f59702cf1d98b504deb284f48f48f6e8
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:20:08 +0200

    Initialize privacy-policy-heading

---
+++ src/components/CodeInput.tsx
@@ -0,0 +1,55 @@
+import React, {useCallback, useState} from 'react';
+
+import {Box} from './Box';
+import {TextInput} from './TextInput';
+
+export interface CodeInputProps {
+  value: string;
+  onChange: (value: string) => void;
+  accessibilityLabel: string;
+}
+
+export const CodeInput = ({value, onChange, accessibilityLabel}: CodeInputProps) => {
+  const onChangeTrimmed = useCallback(text => onChange(text.trim()), [onChange]);
+  const [isFocus, setIsFocus] = useState(false);
+  const onFocus = useCallback(() => setIsFocus(true), []);
+  const onBlur = useCallback(() => setIsFocus(false), []);
+
+  return (
+    <>
+      <Box
+        marginHorizontal="none"
+        borderRadius={9}
+        borderWidth={4}
+        borderColor={isFocus ? 'focus' : 'overlayBackground'}
+      >
+        <Box
+          flex={1}
+          paddingHorizontal="xs"
+          borderWidth={2}
+          borderColor={isFocus ? 'overlayBodyText' : 'gray2'}
+          borderRadius={5}
+        >
+          <TextInput
+            color="bodyText"
+            value={value}
+            onChangeText={onChangeTrimmed}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            autoCorrect={false}
+            autoCompleteType="off"
+            returnKeyType="done"
+            accessibilityLabel={accessibilityLabel}
+            padding="s"
+            maxLength={10}
+            fontSize={26}
+            borderWidth={0}
+            autoCapitalize="characters"
+            fontFamily="Menlo"
+            letterSpacing={5}
+          />
+        </Box>
+      </Box>
+    </>
+  );
+};
---
+++ src/screens/tutorial/views/ItemView.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {StyleSheet, Image, ImageSourcePropType} from 'react-native';
+import {Text, TextMultiline} from 'components';
+import {useI18n} from 'locale';
+import {useAccessibilityAutoFocus} from 'shared/useAccessibilityAutoFocus';
+
+import {TutorialKey} from '../TutorialContent';
+
+export interface ItemViewProps {
+  item: TutorialKey;
+  image: ImageSourcePropType;
+  isActive: boolean;
+}
+
+export const ItemView = ({item, image, isActive}: ItemViewProps) => {
+  const i18n = useI18n();
+  const [, autoFocusRef] = useAccessibilityAutoFocus(isActive);
+
+  return (
+    <>
+      <Image
+        accessible
+        ref={autoFocusRef}
+        style={styles.image}
+        source={image}
+        accessibilityLabel={i18n.translate(`Tutorial.${item}AltText`)}
+      />
+      <Text variant="bodyTitle" color="overlayBodyText" marginBottom="l" accessible accessibilityRole="header">
+        {i18n.translate(`Tutorial.${item}Title`)}
+      </Text>
+      <TextMultiline
+        text={i18n.translate(`Tutorial.${item}`)}
+        variant="bodyText"
+        color="overlayBodyText"
+        marginBottom="l"
+      />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 189,
+    marginBottom: 30,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+});

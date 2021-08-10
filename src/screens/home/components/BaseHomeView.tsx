71b0d4a259a0094136a0ca9cd4c2801b56caa5ef
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:21:41 +0200

    Initialize feature/detox-test-1

---
+++ src/screens/home/components/BaseHomeView.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {StyleSheet, ScrollView} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Box, Header, Icon, IconName} from 'components';
+
+interface BaseHomeViewProps {
+  children?: React.ReactNode;
+  iconName?: IconName;
+  testID?: string;
+}
+
+export const BaseHomeView = ({children, iconName, testID}: BaseHomeViewProps) => {
+  return (
+    <>
+      <SafeAreaView edges={['top']}>
+        <Header />
+      </SafeAreaView>
+      <ScrollView contentContainerStyle={styles.scrollContainer} bounces={false} testID={testID}>
+        <SafeAreaView edges={['left', 'right']}>
+          <Box width="100%" justifyContent="flex-start" marginBottom="-l">
+            <Box style={{...styles.primaryIcon}}>
+              <Icon name={iconName} height={120} width={150} />
+            </Box>
+          </Box>
+          <Box width="100%" flex={1} alignItems="flex-start" justifyContent="flex-start" paddingHorizontal="m">
+            {children}
+          </Box>
+        </SafeAreaView>
+      </ScrollView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  primaryIcon: {marginLeft: -40, marginBottom: 30},
+  scrollContainerWithAnimation: {
+    marginTop: -100,
+  },
+  scrollContainer: {
+    minHeight: '90%',
+    maxWidth: 600,
+    alignItems: 'flex-start',
+  },
+});

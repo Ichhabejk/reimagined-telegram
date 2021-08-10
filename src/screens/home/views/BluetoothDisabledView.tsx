d56a452e1c3549a0d41ecff1724f0fbbc66f8852
kruemelchen39@gmail.com <65755105+Ichhabejk@users.noreply.github.com>
2021-08-10 05:19:27 +0200

    Initialize pre-flight-1594494242828

---
+++ src/screens/home/views/BluetoothDisabledView.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {useI18n} from '@shopify/react-i18n';
+import {Text} from 'components';
+
+import {BaseHomeView} from '../components/BaseHomeView';
+
+export const BluetoothDisabledView = () => {
+  const [i18n] = useI18n();
+
+  return (
+    <BaseHomeView iconName="icon-bluetooth-disabled">
+      <Text variant="bodyTitle" color="bodyText" marginBottom="m" accessibilityRole="header">
+        {i18n.translate('Home.BluetoothDisabled')}
+      </Text>
+      <Text variant="bodyText" color="bodyText" marginBottom="m">
+        {i18n.translate('Home.EnableBluetoothCTA')}
+      </Text>
+    </BaseHomeView>
+  );
+};
